68df65de90df1a7c66b7a7165d550fb5d0c5d5ea
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2023-01-02 00:25:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function ContactBtn(){
+    const button = document.querySelector("button");
+    button.addEventListener("click", ContactBtn);
+    alert("Submitted Succesfully")
+}
+function Button(){
+    const button = document.querySelector("button");
+    button.addEventListener("click", button)
+    alert("Sended")
+}
